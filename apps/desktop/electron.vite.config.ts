0874b5d3fb4e5bc0b49127fe9413a3648d67912f
kazachenka <kazachenkovova2001@gmail.com>
2026-03-31 16:51:04 +0300

    [feature] fix vite config

--- apps/desktop/electron.vite.config.ts
+++ apps/desktop/electron.vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { resolve } from "node:path"
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "")
+  const env = loadEnv(mode, __dirname, "")
 
   return {
     main: {
