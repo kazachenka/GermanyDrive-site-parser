e77aabc84bfa779251379f4902c48a776f1d1ca7
kazachenka <kazachenkovova2001@gmail.com>
2026-08-16 12:48:20 +0300

    [feature] fix bug with get data from mobile.de/ru with vpn

--- apps/desktop/src/main/services/parser-api.service.ts
+++ apps/desktop/src/main/services/parser-api.service.ts
@@ -1,12 +1,28 @@
+import { net } from 'electron'
+
 import { ProductPostItemType } from "@site-parser/shared"
-import {mainApiFetch} from "./utils/fetch.utils";
+import { mainApiFetch } from "./utils/fetch.utils";
 
 export async function getHtmlByUrl(
   payload: string
 ): Promise<any> {
-  const res = await fetch(payload, {
+  const res = await net.fetch(payload, {
+    method: 'GET',
     headers: {
-      'User-Agent': 'Mozilla/5.0',
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+      'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+      'Accept-Language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+      'Sec-Ch-Ua':
+        '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+      'Sec-Ch-Ua-Mobile': '?0',
+      'Sec-Ch-Ua-Platform': '"Windows"',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
+      'Sec-Fetch-User': '?1',
+      'Upgrade-Insecure-Requests': '1',
     },
   })
 
